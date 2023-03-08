c737c8c4e0efc8a851551fa66776828332d13c35
nborzi <nyaraborzi@hotmail.com>
2023-03-08 15:44:59 +0000

    adding a subsegment to useractivities

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -101,6 +101,7 @@ def data_create_message():
   return
 
 @app.route("/api/activities/home", methods=['GET'])
+@xray_recorder.capture('activities_home')
 def data_home():
   data = HomeActivities.run()
   return data, 200
@@ -142,6 +143,7 @@ def data_activities():
   return
 
 @app.route("/api/activities/<string:activity_uuid>", methods=['GET'])
+@xray_recorder.capture('activities_users')
 def data_show_activity(activity_uuid):
   data = ShowActivity.run(activity_uuid=activity_uuid)
   return data, 200
