25e869c191b3464a72eb888b713d4faf55aaa80e
nborzi <nyaraborzi@hotmail.com>
2023-03-30 20:22:14 +0000

    implement create activities

--- aws/json/lambdas/cruddur-post-confirmation.py
+++ aws/json/lambdas/cruddur-post-confirmation.py
@@ -2,42 +2,43 @@ import json
 import psycopg2
 import os
 
-
 def lambda_handler(event, context):
     user = event['request']['userAttributes']
     print('userAttributes')
     print(user)
-    global conn 
 
     user_display_name  = user['name']
     user_email         = user['email']
     user_handle        = user['preferred_username']
     user_cognito_id    = user['sub']
     try:
-        conn = psycopg2.connect(os.getenv('CONNECTION_URL'))
-        cur = conn.cursor()
-        print('entered-try')
-        sql = f"""
-          INSERT INTO public.users (
+      print('entered-try')
+      sql = f"""
+         INSERT INTO public.users (
           display_name, 
           email,
           handle, 
           cognito_user_id
           ) 
-          VALUES(
-          '{user_display_name}', 
-          '{user_email}', 
-          '{user_handle}', 
-          '{user_cognito_id}'
-          )
-          """
-        cur.execute(sql)
-        conn.commit() 
+          VALUES(%s,%s,%s,%s)
+      """
+      print('SQL Statement ----')
+      print(sql)
+      conn = psycopg2.connect(os.getenv('CONNECTION_URL'))
+      cur = conn.cursor()
+      params = [
+        user_display_name,
+        user_email,
+        user_handle,
+        user_cognito_id
+      ]
+      cur.execute(sql,*params)
+      conn.commit() 
 
     except (Exception, psycopg2.DatabaseError) as error:
       print(error)
     finally:
-      global conn 
+      conn = psycopg2.connect(os.getenv('CONNECTION_URL'))
       if conn is not None:
           cur.close()
           conn.close()
