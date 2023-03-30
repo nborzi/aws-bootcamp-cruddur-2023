2243a8f6b1f1042f4650cdb9f5224cdf04884de6
nborzi <nyaraborzi@hotmail.com>
2023-03-30 19:42:55 +0000

    userdatacreated in RDS

--- aws/json/lambdas/cruddur-post-confirmation.py
+++ aws/json/lambdas/cruddur-post-confirmation.py
@@ -2,41 +2,42 @@ import json
 import psycopg2
 import os
 
+
 def lambda_handler(event, context):
     user = event['request']['userAttributes']
     print('userAttributes')
     print(user)
+    global conn 
 
     user_display_name  = user['name']
     user_email         = user['email']
     user_handle        = user['preferred_username']
     user_cognito_id    = user['sub']
     try:
-      print('entered-try')
-      sql = f"""
-         INSERT INTO public.users (
+        conn = psycopg2.connect(os.getenv('CONNECTION_URL'))
+        cur = conn.cursor()
+        print('entered-try')
+        sql = f"""
+          INSERT INTO public.users (
           display_name, 
           email,
           handle, 
           cognito_user_id
           ) 
-        VALUES(
+          VALUES(
           '{user_display_name}', 
           '{user_email}', 
           '{user_handle}', 
           '{user_cognito_id}'
-        )
-      """
-      print('SQL Statement ----')
-      print(sql)
-      conn = psycopg2.connect(os.getenv('CONNECTION_URL'))
-      cur = conn.cursor()
-      cur.execute(sql)
-      conn.commit() 
+          )
+          """
+        cur.execute(sql)
+        conn.commit() 
 
     except (Exception, psycopg2.DatabaseError) as error:
       print(error)
     finally:
+      global conn 
       if conn is not None:
           cur.close()
           conn.close()
